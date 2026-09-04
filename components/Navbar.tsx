55855b1d012063713d13d41428fc610189dec184
Samuel Developer <80781737+samueldeve6@users.noreply.github.com>
2026-09-04 18:33:27 -0500

    dropdown menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,16 @@
-import { Moon } from "lucide-react"
+import { LogOut, Moon, Settings, SquareMenu, User } from "lucide-react"
 import Link from "next/link"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const Navbar = () => {
     return (
@@ -11,10 +21,49 @@ const Navbar = () => {
             <div className="flex items-center gap-4">
                 <Link href="/">Dashboard</Link>
                 <Moon/>
-                <Avatar>
-                    <AvatarImage src="https://github.com/samueldeve6.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/samueldeve6.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent sideOffset={10}>
+                        <DropdownMenuGroup>
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuItem>
+                                <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+                                    Profile
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+                                    Settings
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem variant="destructive">
+                                <LogOut className="h-[1.2rem] w-[1.2rem] mr-2"/>
+                                Logout
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <SquareMenu/>
+                            <span className="sr-only">Open Menu</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem>Menu Item 1</DropdownMenuItem>
+                        <DropdownMenuItem>Menu Item 2</DropdownMenuItem>
+                        <DropdownMenuItem>Menu Item 3</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>        
             </div>
         </nav>
     );

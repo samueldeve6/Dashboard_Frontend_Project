564916832b1fc604c6a3a7b4977718501e9f33f7
Samuel Developer <80781737+samueldeve6@users.noreply.github.com>
2026-09-22 19:41:54 -0500

    fix colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
 
 
 const Homepage = () => {
@@ -8,9 +10,13 @@ const Homepage = () => {
         <AppBarChart />
       </div>
       <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
+      <div className='bg-primary-foreground p-4 rounded-lg'>
+        <AppPieChart />
+      </div>
       <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
-      <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
-      <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2'>Test</div>
+      <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2'>
+        <AppAreaChart />
+      </div>
       <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
     </div>
   );
